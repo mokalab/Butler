e4f5614a8640b615b26a60d73e5861ffa3b7fda2
Pirdad S <pirdad.sakhizada@digiflare.com>
2015-01-29 19:03:29 -0500

    added BaseDialogFragment.

---
+++ ButlerLibrary/src/main/java/com/mokalab/butler/fragments/BaseDialogFragment.java
@@ -0,0 +1,150 @@
+package com.mokalab.butler.fragments;
+
+import android.os.Parcelable;
+import android.support.v4.app.DialogFragment;
+import android.view.View;
+
+import com.mokalab.butler.interfaces.IBundleArgs;
+import com.mokalab.butler.interfaces.IContextHelper;
+import com.mokalab.butler.interfaces.IMrLogger;
+import com.mokalab.butler.interfaces.IViewHelper;
+import com.mokalab.butler.util.ActivityUtils;
+import com.mokalab.butler.util.BundleArgs;
+import com.mokalab.butler.util.MrLogger;
+import com.mokalab.butler.util.ViewUtils;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * TODO: JAVA DOC
+ *
+ * <br><br>
+ * Created by Pirdad S on 2014-07-22.
+ */
+public abstract class BaseDialogFragment extends DialogFragment implements IBundleArgs, IViewHelper, IMrLogger, IContextHelper {
+
+    /* ================== */
+    /* ====== IBundleArgs */
+
+    /**
+     * Get String Type arguments from {@link #getArguments()}.
+     */
+    @Override
+    public String getStringArg(@NotNull String key, String def) {
+
+        return BundleArgs.getStringArg(getArguments(), key, def);
+    }
+
+    /**
+     * Get Integer Type arguments from {@link #getArguments()}.
+     */
+    @Override
+    public int getIntArg(@NotNull String key, int def) {
+
+        return BundleArgs.getIntArg(getArguments(), key, def);
+    }
+
+    /**
+     * Get Integer Array Type arguments from {@link #getArguments()}.
+     */
+    @Override
+    public int[] getIntArrayArg(@NotNull String key, int[] def) {
+
+        return BundleArgs.getIntArrayArg(getArguments(), key, def);
+    }
+
+    /**
+     * Get Boolean Type arguments from {@link #getArguments()}.
+     */
+    @Override
+    public boolean getBooleanArg(@NotNull String key, boolean def) {
+
+        return BundleArgs.getBooleanArg(getArguments(), key, def);
+    }
+
+    /**
+     * Get Parcelable Type arguments from {@link #getArguments()}.
+     */
+    @Override
+    public <T extends Parcelable> T getParcelableArg(@NotNull String key) {
+
+        return BundleArgs.getParcelableArg(getArguments(), key);
+    }
+
+    /**
+     * Get Parcelable ArrayList Type arguments from {@link #getArguments()}.
+     */
+    @Override
+    public <T extends Parcelable> ArrayList<T> getParcelableArrayListArg(@NotNull String key) {
+
+        return BundleArgs.getParcelableArrayListArg(getArguments(), key);
+    }
+
+    /**
+     * Get Serializable Type arguments from {@link #getArguments()}.
+     */
+    @Override
+    public Serializable getSerializableArg(@NotNull String key) {
+
+        return BundleArgs.getSerializableArg(getArguments(), key);
+    }
+
+
+    /* ================== */
+    /* ====== IViewHelper */
+
+
+    @Nullable
+    @Override
+    public <T extends View> T findView(View from, int viewResId) {
+
+        return ViewUtils.findView(from, viewResId);
+    }
+
+
+    /* ================== */
+    /* ====== IMrLogger */
+
+
+    @Override
+    public String getLogTag() {
+
+        return getClass().getSimpleName();
+    }
+
+    @Override
+    public void debug(String message) {
+
+        String tag = getLogTag();
+        MrLogger.debug(tag, message, shouldLog());
+    }
+
+    @Override
+    public void info(String message) {
+
+        String tag = getLogTag();
+        MrLogger.info(tag, message, shouldLog());
+    }
+
+    @Override
+    public void error(String message) {
+
+        String tag = getLogTag();
+        MrLogger.error(tag, message, shouldLog());
+    }
+
+
+    /* ================== */
+    /* ====== IContextHelper */
+
+
+    @Override
+    public boolean isContextValid() {
+
+        return ActivityUtils.isContextValid(getActivity());
+    }
+}
